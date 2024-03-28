10680ca2ad50e11784d565238d284aca843a8474
Gleb <gleb.pastushenko.it@gmail.com>
2024-03-28 18:38:33 +0000

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Team!</h1>
+      <h1 style={{backgroundColor: 'red'}}>Hello, Team!</h1>
       <h2>Let's get it started!</h2>
     </div>
   );
