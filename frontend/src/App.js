c57bfaa2d3dd211cd5ecfa3356d95f53e61d2080
Gleb <gleb.pastushenko.it@gmail.com>
2024-03-28 16:31:08 +0000

    Environment set up

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Team!</h1>
+      <h2>Let's get it started!</h2>
+    </div>
+  );
+}
+
+export default App;
