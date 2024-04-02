03d2cc63f6f49c772fbedd7f6eafca2cda47db79
Gleb <gleb.pastushenko.it@gmail.com>
2024-04-02 06:03:13 +0000

    UserScreen info preparing

--- frontend/src/screens/NewsScreen.jsx
+++ frontend/src/screens/NewsScreen.jsx
@@ -19,3 +19,4 @@ function NewsScreen() {
 }
 
 export default NewsScreen
+
---
+++ frontend/src/users.js
@@ -0,0 +1,32 @@
+users = [
+  {
+    _id: 1,
+    first_name: "Данило",
+    last_name: "Семаньків",
+    farthers_name: "Батькович",
+    birth_date: "10.11.1994",
+    home_address: "м. Івано-Франкіськ, вул. Остафія Дашковича, 32, кв. 27",
+    phone_number: "+380972291381",
+    photo: "1.jpg",
+  },
+  {
+    _id: 2,
+    first_name: "Лариса",
+    last_name: "Іванівна",
+    farthers_name: "Батьківна",
+    birth_date: "03.05.1997",
+    home_address: "м. Черкаси, просп. Шевченка, 135, кв. 60",
+    phone_number: "+380935332018",
+    photo: "2.jpg",
+  },
+  {
+    _id: 3,
+    first_name: "Гліб",
+    last_name: "Пастушенко",
+    farthers_name: "Юрійович",
+    birth_date: "05.02.1985",
+    home_address: "м. Черкаси, вул. Казацька, 10, кв. 72",
+    phone_number: "+380975720012",
+    photo: "3.jpg",
+  },
+]
